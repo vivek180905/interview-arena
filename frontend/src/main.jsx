00e3fc9cba3a6efe8c17278f4f9cf183b2d6c112
vivek180905 <viraj9999singh@gmail.com>
2026-04-23 16:20:38 +0530

    feat: implement MongoDB connection and integrate Clerk for authentication

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/react'
+
+// const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+// if( !PUBLISHABLE_KEY ) {
+//   throw new Error("VITE_CLERK_PUBLISHABLE_KEY is not defined in environment variables");
+// }
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+     <ClerkProvider >
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
