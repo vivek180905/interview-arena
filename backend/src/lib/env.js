3f23fd859d99641b4da61e3a8a07372078b2a75f
vivek180905 <viraj9999singh@gmail.com>
2026-04-24 15:01:37 +0530

    feat: integrate Inngest for user synchronization and management
    - Added Inngest as a dependency to handle serverless functions.
    - Created a new User model for MongoDB to store user information.
    - Implemented user synchronization on creation and deletion events from Clerk.
    - Updated environment variables to include Inngest keys and client URL.
    - Configured CORS to allow requests from the frontend application.
    - Set up Inngest function handler in the Express server.

--- backend/src/lib/env.js
+++ backend/src/lib/env.js
@@ -5,5 +5,14 @@ dotenv.config({quiet: true});
 export const ENV = {
     PORT: process.env.PORT,
     DB_URL: process.env.DB_URL,
-    NODE_ENV: process.env.NODE_ENV
+    NODE_ENV: process.env.NODE_ENV,
+    CLIENT_URL: process.env.CLIENT_URL,
+    // CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    // CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+    STREAM_API_SECRET: process.env.STREAM_API_SECRET,
+    STREAM_API_KEY: process.env.STREAM_API_KEY,
+    INNGEST_EVENT_KEY: process.env.INNGEST_EVENT_KEY,
+    INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY,
+
+
 }
